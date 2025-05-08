759d648dff3cc500338706e7d57f02c509a1810d
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-08 19:06:36 +0000

    feat(projeto): Implementa gerador de ID do projeto (WIP)

--- domain/Projeto/IdProjeto.php
+++ domain/Projeto/IdProjeto.php
@@ -11,6 +11,13 @@ class IdProjeto
         $this->valor = $valor;
     }
 
+    public static function generate(): IdProjeto
+    {
+        // Gerar o UUID do Projeto
+
+        return New static('');
+    }
+
     public function equals($other): bool
     {
         if (!($other instanceof IdProjeto)) {
