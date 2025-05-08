09faa14aed1b8fdab2d7e9620c872d142de43fb0
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-08 19:23:58 +0000

    feat(repositories): Cria model do eloquent para Projeto

---
+++ app/Repositories/Projeto/Projeto.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repositories\Projeto;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Projeto extends Model
+{
+    public const NOME = 'nome';
+    public const DESCRICAO = 'descricao';
+    public const ATIVO = 'ativo';
+    public const ORCAMENTO_DISPONIVEL = 'orcamento_disponivel';
+    public const CRIADO_EM = 'criado_em';
+    public const CRIADOR_ID = 'criador_id';
+
+    protected $table = 'projeto';
+
+    public $timestamps = false;
+
+    protected $keyType = 'string';
+
+    protected $primaryKey = 'id';
+}
