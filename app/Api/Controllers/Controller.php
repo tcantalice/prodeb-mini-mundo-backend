1b383892842f1f80b6a4dd61783736cacf4a4fac
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-09 07:09:04 +0000

    style(api/controllers): Remove comentários desnecessários

--- app/Api/Controllers/Controller.php
+++ app/Api/Controllers/Controller.php
@@ -6,11 +6,8 @@ use Psr\Log\LoggerInterface;
 
 abstract class Controller
 {
-    // Base controller for the API
-    // This can be extended by other controllers to share common functionality
-
     public function __construct(protected LoggerInterface $logger)
     {
-        // Initialization code can go here
+        //
     }
 }
