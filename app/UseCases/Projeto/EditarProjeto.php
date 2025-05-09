8803e38f83d35778cd9a57cd8a149d343c5e3058
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-09 18:32:43 +0000

    feat(useCases/projeto): Cria caso de uso de edição de projeto

---
+++ app/UseCases/Projeto/EditarProjeto.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\UseCases\Projeto;
+
+use Domain\Projeto\Contracts\ProjetoRepository;
+use Domain\Projeto\Exceptions\ProjetoNaoEncontradoException;
+use Psr\Log\LoggerInterface;
+
+class EditarProjeto
+{
+    public function __construct(
+        private LoggerInterface $logger,
+        private ProjetoRepository $projetoRepo
+    ) {
+        //
+    }
+
+    public function execute(EditarProjetoDTO $input)
+    {
+        $projeto = $this->projetoRepo->find($input->id);
+
+        if ($projeto === null) {
+            throw new ProjetoNaoEncontradoException();
+        }
+
+        if ($input->nome !== null) {
+            $projeto->setNome($input->nome);
+        }
+
+        if ($input->descricao !== null) {
+            $projeto->setDescricao($input->descricao);
+        }
+
+        if ($input->orcamento !== null) {
+            $projeto->setOrcamento($input->orcamento);
+        }
+
+        if ($input->ativo !== null) {
+            $input->ativo ? $projeto->ativar() : $projeto->inativar();
+        }
+
+        try {
+            $this->projetoRepo->save($projeto);
+        } catch (\Throwable $th) {
+            throw new \Exception('Erro ao editar o projeto: ' . $th->getMessage());
+        }
+    }
+}
