3fade8b804eeaf3e5a874169a8eb022abcd23b6f
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-09 04:22:40 +0000

    refactor(useCases/projeto): Adiciona referência ao usuário criador

--- app/UseCases/Projeto/CriarProjetoDTO.php
+++ app/UseCases/Projeto/CriarProjetoDTO.php
@@ -5,8 +5,9 @@ namespace App\UseCases\Projeto;
 class CriarProjetoDTO
 {
     public function __construct(
-        public readonly string $nome,
-        public readonly ?string $descricao = null,
+        public readonly String $nome,
+        public readonly String $refUsuarioCriador,
+        public readonly ?String $descricao = null,
         public readonly ?float $orcamento = null,
     ) {}
 }
