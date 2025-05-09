2e9a059d74def63edb53a2e99887944ee108577c
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-09 15:42:12 +0000

    fix(useCases/projeto): Corrige tipagem dos atributos

--- app/UseCases/Projeto/ProjetoDTO.php
+++ app/UseCases/Projeto/ProjetoDTO.php
@@ -5,13 +5,13 @@ namespace App\UseCases\Projeto;
 class ProjetoDTO
 {
     public function __construct(
-        public readonly int $id,
+        public readonly String $id,
         public readonly string $nome,
         public readonly ?string $descricao,
         public readonly ?float $orcamento,
         public readonly bool $ativo,
         public readonly CriadorProjetoDTO $criadoPor,
-        public readonly \DateTimeImmutable $criadoEm
+        public readonly \DateTimeInterface $criadoEm
     ) {
         //
     }
