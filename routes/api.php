3e2e1f315f72f3d05ecc854b9b9c4d5532fe2e08
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-09 05:18:08 +0000

    refactor(routes): Remove prefixo do roteamento da api

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use App\Api\Controllers\ProjetoController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('api')
-    ->prefix('api')
     ->group(function () {
         Route::post('/projetos', [ProjetoController::class, 'create']);
     });
