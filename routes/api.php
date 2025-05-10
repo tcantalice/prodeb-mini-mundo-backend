1a3aab2ad52691cfeee6f70de2eb843b7d1ed028
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-10 20:38:54 +0000

    feat(routes/api): Cria rota de consulta pelo id

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,6 @@ Route::middleware('api')
     ->group(function () {
         Route::post('/projetos', [ProjetoController::class, 'create']);
         Route::get('/projetos', [ProjetoController::class, 'list']);
+        Route::get('/projetos/{idProjeto}', [ProjetoController::class, 'get']);
         Route::put('/projetos/{idProjeto}', [ProjetoController::class, 'update']);
     });
