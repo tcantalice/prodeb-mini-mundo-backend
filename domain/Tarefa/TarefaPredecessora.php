d001b49b89e0c8d55ae0bac6cf59c53f86b6dc7d
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-16 17:54:16 +0000

    refactor(tarefa): Remove atributo booleano 'concluida' e adiciona atributo nulável 'concluidaEm'

--- domain/Tarefa/TarefaPredecessora.php
+++ domain/Tarefa/TarefaPredecessora.php
@@ -4,14 +4,19 @@ namespace Domain\Tarefa;
 
 class TarefaPredecessora
 {
-    public function __construct(private IdTarefa $ref, private bool $concluida)
+    public function __construct(private IdTarefa $ref, private ?\DateTimeInterface $concluidaEm)
     {
         //
     }
 
+    public function getConcluidaEm(): ?\DateTimeInterface
+    {
+        return $this->concluidaEm;
+    }
+
     public function isConcluida(): bool
     {
-        return $this->concluida;
+        return $this->getConcluidaEm() !== null;
     }
 
     public function getRef(): IdTarefa
