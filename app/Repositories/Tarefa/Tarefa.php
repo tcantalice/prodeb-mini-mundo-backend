15d466a5d9f75c900dbfa83f1999c438e57d0fb0
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-16 00:52:32 +0000

    feat(repositories/tarefa): Cria model do eloquent de tarefa
    Implementa método para definir o criador
    Implementa método para consultar obter o criador já convertido em VO

---
+++ app/Repositories/Tarefa/Tarefa.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Repositories\Tarefa;
+
+use App\Repositories\Usuario\Usuario;
+use Domain\Tarefa\CriadorTarefa;
+use Illuminate\Database\Eloquent\Model;
+
+class Tarefa extends Model
+{
+    public const ID = 'id';
+    public const UUID = 'uuid';
+    public const DESCRICAO = 'descricao';
+    public const DATA_HORA_INICIO = 'data_hora_inicio';
+    public const DATA_HORA_FIM = 'data_hora_fim';
+    public const CRIADOR_ID = 'usuario_criador_id';
+    public const PROJETO_ID = 'projeto_id';
+    public const CRIADO_EM = 'criado_em';
+
+    protected $table = 'tarefa';
+
+    public $timestamps = false;
+
+    protected $keyType = 'int';
+
+    public $incrementing = true;
+
+    protected $primaryKey = self::ID;
+
+    public $fillable = [
+        self::DESCRICAO,
+        self::DATA_HORA_INICIO,
+        self::DATA_HORA_FIM,
+        self::CRIADO_EM
+    ];
+
+    protected $casts = [
+        self::CRIADO_EM => 'datetime'
+    ];
+
+    public function relationCriador()
+    {
+        return $this->belongsTo(Usuario::class, self::CRIADOR_ID);
+    }
+
+    public function setCriador(string $id)
+    {
+        $this->relationCriador()->associate(Usuario::where(Usuario::LOGIN, $id)->first());
+    }
+
+    public function getCriador(): CriadorTarefa
+    {
+        return new CriadorTarefa(
+            $this->relationCriador->getAttribute(Usuario::ID),
+            $this->relationCriador->getAttribute(Usuario::NOME)
+        );
+    }
+}
