e4298f258c019677844219e6908b31f2ca165b93
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-16 19:36:17 +0000

    feat(tarefa): Adiciona assinatura do método find no repositóri

--- domain/Tarefa/Contracts/TarefaRepository.php
+++ domain/Tarefa/Contracts/TarefaRepository.php
@@ -8,6 +8,8 @@ interface TarefaRepository
 {
     function save(Tarefa $tarefa): void;
 
+    function find(string $id): ?Tarefa;
+
     function findAllByDependencia(string $tarefaRef): array;
 
     function findAllByProjeto(string $projetoRef): array;
