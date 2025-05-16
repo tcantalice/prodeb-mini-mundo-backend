4f1c19e1847a011aa01068f552bb2b8745e96d42
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-16 19:38:41 +0000

    feat(useCases/tarefa): Cria DTO do input de dados do caso de uso de criação de tarefa

--- app/UseCases/Tarefa/CriarTarefaDTO.php
+++ app/UseCases/Tarefa/CriarTarefaDTO.php
@@ -6,7 +6,7 @@ class CriarTarefaDTO
 {
     public function __construct(
         public readonly string $descricao,
-        public readonly string $projetoRef,
+        public readonly string $refProjeto,
         public readonly string $refUsuarioCriador,
         public readonly ?string $refTarefaPredecessora = null
     ) {
