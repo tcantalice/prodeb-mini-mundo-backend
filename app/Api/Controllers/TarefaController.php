629190197a1347f3405be565d1cb5387f702a531
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-20 17:58:12 +0000

    feat(api/controllers): Implementa action para listar as tarefas pelo projeto (WIP)

--- app/Api/Controllers/TarefaController.php
+++ app/Api/Controllers/TarefaController.php
@@ -6,6 +6,8 @@ use App\Api\Requests\Tarefa\CriarTarefaRequest;
 use App\UseCases\Projeto\ConsultarProjeto;
 use App\UseCases\Tarefa\CriarTarefa;
 use App\UseCases\Tarefa\CriarTarefaDTO;
+use App\UseCases\Tarefa\ListarTarefasProjeto;
+use App\UseCases\Tarefa\TarefaOutput;
 use Illuminate\Support\Facades\Auth;
 
 class TarefaController extends Controller
@@ -35,5 +37,23 @@ class TarefaController extends Controller
         } catch(\Throwable $th) {
             return response()->json(['message' => 'Erro ao cadastrar tarefa'], 500);
         }
+    }
+
+    public function listByProjeto(string $projetoId, ListarTarefasProjeto $listarTarefasProjetoUseCase)
+    {
+        $tarefas = $listarTarefasProjetoUseCase->execute($projetoId);
+
+        return $this->makeSuccessResponse(collect($tarefas)->map(function (TarefaOutput $tarefa) {
+            return [
+                "id" => $tarefa->id,
+                "descricao" => $tarefa->descricao,
+                "projeto" => $tarefa->projetoRef,
+                "status" => [
+                    "iniciado" => $tarefa->dataInicio !== null,
+                    "finalizado" => $tarefa->dataFim !== null,
+                ],
+                "tarefa_predecessora" => $tarefa->dependeDe
+            ];
+        })->toArray());
     }
 }
