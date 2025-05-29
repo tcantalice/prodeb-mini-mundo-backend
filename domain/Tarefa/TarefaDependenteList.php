b4584dacac0520da35e7cb3c8f254e76791c1334
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-29 17:24:24 +0000

    feat(tarefa): Implementa construtor da lista de dependentes de uma tarefa

--- domain/Tarefa/TarefaDependenteList.php
+++ domain/Tarefa/TarefaDependenteList.php
@@ -2,16 +2,38 @@
 
 namespace Domain\Tarefa;
 
+use InvalidArgumentException;
+
 class TarefaDependenteList
 {
+
+    private readonly IdTarefa $dependenciaRef;
+
     /**
      * @var Tarefa[]
      */
     private readonly array $tarefas;
 
-    public function __construct()
+    public function __construct(IdTarefa $dependenciaRef, array $tarefas)
     {
+        $this->dependenciaRef = $dependenciaRef;
+        $this->tarefas = [];
 
+        foreach ($tarefas as $tarefa) {
+            if (!$tarefa instanceof Tarefa) {
+                throw new InvalidArgumentException(
+                    'Elementos no array $tarefas devem ser instâncias de Tarefa'
+                );
+            }
+
+            if ($tarefa->hasDependencia() && !$tarefa->dependeDe()->getRef()->equals($dependenciaRef)) {
+                throw new InvalidArgumentException(
+                    "A tarefa {$tarefa->getID()} não depende de {$dependenciaRef}"
+                );
+            }
+
+            $this->tarefas[] = $tarefa;
+        }
     }
 
     /**
