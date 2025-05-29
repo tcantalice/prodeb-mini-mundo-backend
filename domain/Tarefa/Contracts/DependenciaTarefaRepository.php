f36d1b02cf9787af2fcadfc0faca661564a49b86
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-29 15:50:28 +0000

    feat(tarefa/contracts): Adiciona assinaturas de métodos ao repositório de dependência
    Adiciona assinatura da busca por dependentes
    Adiciona assinatura da adição de dependência
    Adiciona assinatura da remoção de dependência

--- domain/Tarefa/Contracts/DependenciaTarefaRepository.php
+++ domain/Tarefa/Contracts/DependenciaTarefaRepository.php
@@ -2,7 +2,13 @@
 
 namespace Domain\Tarefa\Contracts;
 
+use Domain\Tarefa\TarefaDependenteList;
+
 interface DependenciaTarefaRepository
 {
-    //
+    function findAllDependentes(string $id): TarefaDependenteList;
+
+    function addDependencia(Tarefa $dependente): void;
+
+    function removeDependencia(Tarefa $dependente): void;
 }
