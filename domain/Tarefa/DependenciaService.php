d6fe4647604741afdd16bc71635244b9508b7f05
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-29 15:44:17 +0000

    feat(tarefa): Implementar service de dependência

---
+++ domain/Tarefa/DependenciaService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Domain\Tarefa;
+
+class DependenciaService
+{
+    public function adicionarDependente(Tarefa $dependencia, Tarefa $dependente)
+    {
+        // TODO: Verificar se são a mesma tarefa
+        // TODO: Verificar se a tarefa já possui dependência.
+        // TODO: Adicionar dependencia à tarefa
+    }
+}
