7c0d7a10b67c0f86d5d1a6a0dfc588c517b646a3
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-29 16:39:26 +0000

    feat(tarefa): Implementa método para adicionar dependência no serviço (WIP)

--- domain/Tarefa/DependenciaService.php
+++ domain/Tarefa/DependenciaService.php
@@ -2,12 +2,25 @@
 
 namespace Domain\Tarefa;
 
+use Domain\Tarefa\Contracts\DependenciaTarefaRepository;
+
 class DependenciaService
 {
-    public function adicionarDependente(Tarefa $dependencia, Tarefa $dependente)
+    public function __construct(private DependenciaTarefaRepository $dependenciaRepo)
     {
-        // TODO: Verificar se são a mesma tarefa
-        // TODO: Verificar se a tarefa já possui dependência.
-        // TODO: Adicionar dependencia à tarefa
+        //
+    }
+
+    public function adicionarDependente(Tarefa $tarefa, Tarefa $dependencia)
+    {
+        if ($tarefa->equals($dependencia)) {
+            throw new \Exception('Uma tarefa não pode ser dependente dela mesma');
+        }
+
+        if ($tarefa->hasDependencia()) {
+            throw new \Exception('A tarefa já possui dependência');
+        }
+
+        $this->dependenciaRepo->addDependencia($tarefa, $dependencia);
     }
 }
