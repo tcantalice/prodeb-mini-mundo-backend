ea9aa2c473633d4aeba7cab424b74e0bae32cb7f
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2025-05-30 21:20:52 +0000

    refactor(api/controllers): Implementar conversão parcial do output de tarefa para array
    Altera retornos para usar conversor parcial
    Adiciona retorno da tarefa na criação

--- app/Api/Controllers/TarefaController.php
+++ app/Api/Controllers/TarefaController.php
@@ -30,14 +30,14 @@ class TarefaController extends Controller
         try {
             $consultarProjetoUseCase->execute($idProjeto);
 
-            $criarTarefaUseCase->execute(new CriarTarefaInput(
+            $tarefa = $criarTarefaUseCase->execute(new CriarTarefaInput(
                 $request->input('descricao'),
                 $idProjeto,
                 Auth::user()->getAuthIdentifier(),
                 $request->input('depende_de')
             ));
 
-            return $this->makeSuccessResponse(statusCode: 201);
+            return $this->makeSuccessResponse($this->parsePartialTarefaOutputToArray($tarefa),  201);
         } catch(\Throwable $th) {
             return response()->json(['message' => 'Erro ao cadastrar tarefa'], 500);
         }
@@ -47,28 +47,17 @@ class TarefaController extends Controller
     {
         $tarefas = $listarTarefasProjetoUseCase->execute($projetoId);
 
-        return $this->makeSuccessResponse(collect($tarefas)->map(function (TarefaOutput $tarefa) {
-            return [
-                "id" => $tarefa->id,
-                "descricao" => $tarefa->descricao,
-                "iniciada_em" => $tarefa->dataInicio ? $this->serializeDateTime($tarefa->dataInicio) : null,
-                "finalizada_em" => $tarefa->dataFim ? $this->serializeDateTime($tarefa->dataFim) : null,
-                "depende_de" => $tarefa->dependeDe
-            ];
-        })->toArray());
+        return $this->makeSuccessResponse(collect($tarefas)
+            ->map(function (TarefaOutput $tarefa) {
+                return $this->parsePartialTarefaOutputToArray($tarefa);
+            })->toArray());
     }
 
     public function changeStatus(string $tarefaId, AlterarStatus $alterarStatusUseCase)
     {
         $tarefa = $alterarStatusUseCase->execute($tarefaId);
 
-        return $this->makeSuccessResponse([
-            "id" => $tarefa->id,
-            "descricao" => $tarefa->descricao,
-            "iniciada_em" => $tarefa->dataInicio ? $this->serializeDateTime($tarefa->dataInicio) : null,
-            "finalizada_em" => $tarefa->dataFim ? $this->serializeDateTime($tarefa->dataFim) : null,
-            "depende_de" => $tarefa->dependeDe
-        ]);
+        return $this->makeSuccessResponse($this->parsePartialTarefaOutputToArray($tarefa));
     }
 
     public function changeDependencia(
@@ -83,12 +72,17 @@ class TarefaController extends Controller
             ? $adicionarDependenciaUseCase->execute($tarefaId, $dependenciaId)
             : $removerDependenciaUseCase->execute($tarefaId);
 
-        return $this->makeSuccessResponse([
-            "id" => $tarefa->id,
-            "descricao" => $tarefa->descricao,
-            "iniciada_em" => $tarefa->dataInicio ? $this->serializeDateTime($tarefa->dataInicio) : null,
-            "finalizada_em" => $tarefa->dataFim ? $this->serializeDateTime($tarefa->dataFim) : null,
-            "depende_de" => $tarefa->dependeDe
-        ]);
+        return $this->makeSuccessResponse($this->parsePartialTarefaOutputToArray($tarefa));
+    }
+
+    private function parsePartialTarefaOutputToArray(TarefaOutput $output): array
+    {
+        return [
+            "id"            => $output->id,
+            "descricao"     => $output->descricao,
+            "iniciada_em"   => $output->dataInicio ? $this->serializeDateTime($output->dataInicio) : null,
+            "finalizada_em" => $output->dataFim ? $this->serializeDateTime($output->dataFim) : null,
+            "depende_de"    => $output->dependeDe
+        ];
     }
 }
